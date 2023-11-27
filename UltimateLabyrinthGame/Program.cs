b71090db2eef074029754aaced02ea826296d0e5
LuxRegina <erika.s.hedstrom@gmail.com>
2023-11-27 10:33:06 +0100

    Created 10 rooms.

--- UltimateLabyrinthGame/Program.cs
+++ UltimateLabyrinthGame/Program.cs
@@ -1,36 +1,5 @@
 ﻿namespace UltimateLabyrinthGame
 {
-    public class Room
-    {
-        public static int NoDoor = -997;
-        int self;
-        string title;
-        string text;
-        int north, east, south, west;
-        public Room(int self, string name, string text, int N, int E, int S, int W)
-        {
-            this.self = self; this.title = name; this.text = text;
-            north = N; east = E; south = S; west = W;
-        }
-        public string Title { get { return title; } }
-        public string Text { get { return text; } }
-        public int North { get { return north; } set { north = value; } }
-        public int East { get { return east; } set { east = value; } }
-        public int South { get { return south; } set { south = value; } }
-        public int West { get { return west; } set { west = value; } }
-        public string Directions
-        {
-            get
-            {
-                string dir = "Det går dörrar till:\n";
-                if (north != NoDoor) dir += "  w - norr\n";
-                if (east != NoDoor) dir += "  d - öster\n";
-                if (south != NoDoor) dir += "  s - söder\n";
-                if (west != NoDoor) dir += "  a - väster\n";
-                return dir;
-            }
-        }
-    }
     public class UltimateLabyrinth
     {
         static Room help = new Room(-1, "Help",  // Rename what buttons to press.
