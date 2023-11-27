fd60bfd6398da1011511d7c4d3f1f8f998ad93cd
ViktorOlausson <02viktorolausson@gmail.com>
2023-11-27 09:11:36 +0100

    Namespace OSV

--- UltimateLabyrinthGame/Program.cs
+++ UltimateLabyrinthGame/Program.cs
@@ -1,2 +1,10 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿namespace UltimateLabyrinthGame
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
