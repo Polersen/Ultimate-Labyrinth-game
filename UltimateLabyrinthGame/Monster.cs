b39d8e9c1090c2f3f4e8f1c5103707dba914e6df
Anton Örtlund <anton.ortlund@gmail.com>
2023-11-28 11:23:01 +0100

    Created monster class

---
+++ UltimateLabyrinthGame/Monster.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UltimateLabyrinthGame
+{
+    internal class Monster
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Weakness { get; set; }
+
+        public Monster(string name, string desc, string weak)
+        {
+            this.Name = name;
+            this.Description = desc;
+            this.Weakness = weak;
+        }
+    }
+}
